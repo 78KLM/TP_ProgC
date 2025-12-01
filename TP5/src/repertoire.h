22120ada036f824a0c53de0f0a61c6181e6bdf94
78KLM <159271563+78KLM@users.noreply.github.com>
2025-12-01 15:26:46 +0100

    Update repertoire.h

---
+++ TP5/src/repertoire.h
@@ -0,0 +1,8 @@
+#ifndef REPERTOIRE_H
+#define REPERTOIRE_H
+
+void lire_dossier(const char *nom);
+void lire_dossier_recursif(const char *nom);
+void lire_dossier_iteratif(const char *nom);
+
+#endif
